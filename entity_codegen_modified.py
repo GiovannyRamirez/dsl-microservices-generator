91c009476d4ed65ff6cee0cab7c819baccd3e3b5
GiovannyRamirez <giovannyramirezs@hotmail.com>
2022-11-06 20:13:29 -0500

    Add support to generate main files

--- entity_codegen_modified.py
+++ entity_codegen_modified.py
@@ -8,19 +8,7 @@ import jinja2
 from entity_test import get_entity_mm
 
 
-def main(debug=False, file_name=False):
-
-    templates_folder = "./templates/"
-    entities_folder = "./entidades/"
-    general_folder = "./proyecto_generado/"
-    folder_to_export = general_folder + file_name.split(".ent")[0] + 's'
-
-    this_folder = dirname(__file__)
-
-    entity_mm = get_entity_mm(debug)
-
-    # Build Entity model from file_name file
-    entity_model = entity_mm.model_from_file(join(this_folder, entities_folder + file_name))
+def main(debug=False, file_name=False, files=False):
 
     def is_entity(n):
         """
@@ -40,9 +28,18 @@ def main(debug=False, file_name=False):
                 'string': 'String'
         }.get(s.name, s.name)
 
+    templates_folder = "./templates/"
+    entities_folder = "./entidades/"
+    general_folder = "./proyecto_generado/"
+
+    if file_name:
+        folder_to_export = general_folder + file_name.split(".ent")[0] + 's'
+    else:
+        folder_to_export = general_folder
+
     # Create output folder
+    this_folder = dirname(__file__)
     srcgen_folder = join(this_folder, folder_to_export)
-    main_folder = join(this_folder, general_folder)
     if not exists(srcgen_folder):
         mkdir(srcgen_folder)
 
@@ -53,74 +50,75 @@ def main(debug=False, file_name=False):
         lstrip_blocks=True)
 
     # Register filter for mapping Entity type names to Java type names.
-
     jinja_env.tests['entity'] = is_entity
-
     jinja_env.filters['javatype'] = javatype
 
-    # Load nginx template
-    template = jinja_env.get_template(templates_folder+'nginx.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [nginx.conf]
+    entity_mm = get_entity_mm(debug)  
+
+    if not file_name:
+        main_folder = join(this_folder, general_folder)
+        # Load nginx template
+        template = jinja_env.get_template(templates_folder+'nginx.template')
+        # For each entity add its respectively route in file [nginx.conf]
         with open(join(main_folder,
             "nginx.conf"), 'w') as f:
-            f.write(template.render(entity=entity))
+            f.write(template.render(entities=files))
 
-    # Load docker-compose template
-    template = jinja_env.get_template(templates_folder+'docker-compose.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [docker-compose.yml]
+        # Load docker-compose template
+        template = jinja_env.get_template(templates_folder+'docker-compose.template')
+        # For each entity add its respectively build and link in file [docker-compose.yml]
         with open(join(main_folder,
             "docker-compose.yml"), 'w') as f:
-            f.write(template.render(entity=entity))
+            f.write(template.render(entities=files))
 
-    # Load README template
-    template = jinja_env.get_template(templates_folder+'README.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [README.md]
+        # Load README template
+        template = jinja_env.get_template(templates_folder+'README.template')
+        # For each entity add its respectively field in file [README.md]
         with open(join(main_folder,
             "README.md"), 'w') as f:
-            f.write(template.render(entity=entity))
+            f.write(template.render(entities=files))
+    else:
+        # Build Entity model from file_name file
+        entity_model = entity_mm.model_from_file(join(this_folder, entities_folder + file_name))
+        # Load app template
+        template = jinja_env.get_template(templates_folder+'app.template')
+        for entity in entity_model.entities:
+            # For each entity generate file [app.py]
+            with open(join(srcgen_folder,
+                "app.py"), 'w') as f:
+                f.write(template.render(entity=entity))
 
-    # Load app template
-    template = jinja_env.get_template(templates_folder+'app.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [app.py]
-        with open(join(srcgen_folder,
-            "app.py"), 'w') as f:
-            f.write(template.render(entity=entity))
+        # Load config_app template
+        template = jinja_env.get_template(templates_folder+'config_app.template')
+        for entity in entity_model.entities:
+            # For each entity generate file [config_app.py]
+            with open(join(srcgen_folder,
+                "config_app.py"), 'w') as f:
+                f.write(template.render(entity=entity))
 
-    # Load config_app template
-    template = jinja_env.get_template(templates_folder+'config_app.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [config_app.py]
-        with open(join(srcgen_folder,
-            "config_app.py"), 'w') as f:
-            f.write(template.render(entity=entity))
+        # Load models template
+        template = jinja_env.get_template(templates_folder+'models.template')
+        for entity in entity_model.entities:
+            # For each entity generate file [models.py]
+            with open(join(srcgen_folder,
+                "models.py"), 'w') as f:
+                f.write(template.render(entity=entity))
 
-    # Load models template
-    template = jinja_env.get_template(templates_folder+'models.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [models.py]
-        with open(join(srcgen_folder,
-            "models.py"), 'w') as f:
-            f.write(template.render(entity=entity))
+        # Load requerimientos template
+        template = jinja_env.get_template(templates_folder+'requerimientos.template')
+        for entity in entity_model.entities:
+            # For each entity generate file [requerimientos.txt]
+            with open(join(srcgen_folder,
+                "requerimientos.txt"), 'w') as f:
+                f.write(template.render(entity=entity))
 
-    # Load requerimientos template
-    template = jinja_env.get_template(templates_folder+'requerimientos.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [requerimientos.txt]
-        with open(join(srcgen_folder,
-            "requerimientos.txt"), 'w') as f:
-            f.write(template.render(entity=entity))
-
-    # Load Dockerfile template
-    template = jinja_env.get_template(templates_folder+'Dockerfile.template')
-    for entity in entity_model.entities:
-        # For each entity generate file [Dockerfile]
-        with open(join(srcgen_folder,
-            "Dockerfile"), 'w') as f:
-            f.write(template.render(entity=entity))
+        # Load Dockerfile template
+        template = jinja_env.get_template(templates_folder+'Dockerfile.template')
+        for entity in entity_model.entities:
+            # For each entity generate file [Dockerfile]
+            with open(join(srcgen_folder,
+                "Dockerfile"), 'w') as f:
+                f.write(template.render(entity=entity))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from os import listdir, path
-from entity_codegen_modified import main
+from entity_codegen_modified import main as generateFiles
 
 folder = path.dirname(__file__)
 entidades = listdir(folder + "/entidades/")
+main_files = {}
 
 for entity in entidades:
-    main(False, entity)
+    main_files[entity.split(".ent")[0]] = entity.split(".ent")[0]
+    generateFiles(False, entity, False)
+
+generateFiles(False, False, main_files)
